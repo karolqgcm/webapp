b946509667d2fe24eba8a74d47bf528e4738bce3
kqueiroz <kqueiroz@nd.edu>
2016-02-11 11:33:27 -0500

    hw4

--- Homeworks/hw4/password.js
+++ Homeworks/hw4/password.js
@@ -2,7 +2,32 @@
 
 /* Implement String.validate() here */
 
+var validEx = []
 
+String.prototype.validate2=function (min, max, valid_exp, invalid_exp){
+    if(this.length < min && this.length>max){
+        console.log("min")
+        return false;
+    }else{
+        var val_regex = new RegExp(valid_exp, 'g');
+
+        if (!this.match(val_regex)) {
+            console.log("nval");
+            return false;
+        } else {
+            var inval_regex = new RegExp(invalid_exp, 'g');
+            if (this.match(inval_regex)) {
+                console.log("inv")
+                return false;
+            } else {
+                console.log("val");
+                return true;
+            }
+
+        }
+    }
+
+}
 
 
 
@@ -18,3 +43,5 @@ var test_passwords = [
 ];
 
 
+console.log(test_passwords[1].length);
+console.log(test_passwords[5].validate2(8,20,"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*]).{","[^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*]).{)]"));
