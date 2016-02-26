cd93fea521d1560d546a3e945ca38c8c7d76ccd9
kqueiroz <kqueiroz@nd.edu>
2016-02-26 17:40:29 -0500

    hw4

---
+++ Homeworks/Hw6/popup/popup.js
@@ -0,0 +1,3 @@
+/**
+ * Created by kqueiroz on 2/26/16.
+ */
---
+++ Homeworks/Hw6/remove-list-item/remove-item.js
@@ -0,0 +1,6 @@
+// This is supposed to work on simple.html
+// in the homework 6 starter code
+
+// Write code to remove an item on double click
+// add handler to the ul
+// print the item's text() and html() to the console
